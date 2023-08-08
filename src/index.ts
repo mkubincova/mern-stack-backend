53ae8637fbeae5aec9d93908f571c42a4b34e503
mkubincova <mkubincova@proton.me>
2023-08-08 12:43:42 +0200

    feat: origins

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 const uri = process.env.MONGO_URI || '';
 
 // cors options
-const allowedOrigins = ['http://localhost:5173', 'https://mern-stack-frontend-neon.vercel.app'];
+const allowedOrigins = ['http://localhost:5173', 'https://mern-stack-frontend-neon.vercel.app', 'https://mern-stack-frontend-9jc9jqll3-mkubincova.vercel.app'];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins
