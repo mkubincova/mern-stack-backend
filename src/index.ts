5da58d8861c88b6920783bdbc721be7037639cea
mkubincova <mkubincova@proton.me>
2023-08-08 13:17:11 +0200

    fix: origins

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 const uri = process.env.MONGO_URI || '';
 
 // cors options
-const allowedOrigins = ['http://localhost:5173', 'https://mern-stack-frontend-neon.vercel.app', 'https://mern-stack-frontend-9jc9jqll3-mkubincova.vercel.app'];
+const allowedOrigins = ['http://localhost:5173', 'https://mern-stack-frontend-neon.vercel.app'];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins
@@ -24,6 +24,16 @@ app.use(express.json());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log(req.path, req.method);
+  next();
+});
+
+// Add a middleware to set CORS headers
+app.use((req: Request, res: Response, next: NextFunction) => {
+  // Set the necessary CORS headers
+  res.header('Access-Control-Allow-Origin', req.header('origin'));
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   next();
 });
 
